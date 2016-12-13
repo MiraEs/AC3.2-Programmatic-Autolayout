e42f378c7c98f5a164591d32c8bf0f54634023b9
Mira Estil <imz.estil@gmail.com>
2016-12-13 15:24:00 -0500

    updates

--- AC3.2-Programmatic-Autolayout/AC3.2-Programmatic-Autolayout/ViewController.swift
+++ AC3.2-Programmatic-Autolayout/AC3.2-Programmatic-Autolayout/ViewController.swift
@@ -9,17 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    public override func layoutSubviews() {
+        let blueWidth = "H:[tobias(200.0)]"
+        let blueHight = "V:[tobias(200.0)]"
+        let views = ["tobias" : blueView,
+                     "view" : self.view]
+        blueView.translatesAutoresizingMaskIntoConstraints = false
+        let blueWidthConstraints = NSLayoutConstraint.constraints(withVisualFormat: blueWidth, options: .alignAllCenterX, metrics: nil, views: views)
+        
+        let blueHeightConstraints = NSLayoutConstraint.constraints(withVisualFormat: blueHight, options: .alignAllCenterY, metrics: nil, views: views)
+        
+        //align x and y
+        let blueXConstraints = NSLayoutConstraint.constraints(withVisualFormat: <#T##String#>, options: .alignAllCenterX, metrics: nil, views: views)
+        NSLayoutConstraint.activate(blueHeightConstraints)
+        NSLayoutConstraint.activate(blueWidthConstraints)
+        
+    }
+    
 }
 
